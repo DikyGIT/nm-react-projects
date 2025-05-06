6d7bc28b9c3b3c9e788e81717093e473079984a7
DikyGIT <dikyoga27@gmail.com>
2025-05-06 14:09:14 +0700

    first commit

---
+++ 1-react-loadmore/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="load-more">
+      <div className="container">
+        <h1 className="title">React Load More</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
